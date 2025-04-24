7443120ac06fd4ce07c691791685fdc156ff60fa
Pablo Barra Urtubia <pbarrau.90@icloud.com>
2025-04-24 18:40:03 -0400

    Se modifica el mosaico

--- src/components/CategoriasMosaico.jsx
+++ src/components/CategoriasMosaico.jsx
@@ -2,38 +2,38 @@ import React from "react";
 
 const categorias = [
   {
-    nombre: "Ropa",
-    imagen: "https://images.unsplash.com/photo-1612423284934-9ff25e554ec4?auto=format&fit=crop&w=800&q=80",
+    nombre: "Moda independiente",
+    imagen: "https://images.unsplash.com/photo-1687405182302-f1b28707c854?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     colSpan: 2,
     rowSpan: 2,
   },
   {
-    nombre: "Tecnología",
-    imagen: "https://images.unsplash.com/photo-1518779578993-ec3579fee39f?auto=format&fit=crop&w=800&q=80",
+    nombre: "Hecho a mano",
+    imagen: "https://images.unsplash.com/photo-1474625121024-7595bfbc57ac?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzR8fGhlY2hvJTIwYSUyMG1hbm98ZW58MHx8MHx8fDA%3D",
     colSpan: 2,
     rowSpan: 1,
   },
   {
-    nombre: "Hogar",
-    imagen: "https://images.unsplash.com/photo-1600585154340-be6161a56a0c?auto=format&fit=crop&w=800&q=80",
+    nombre: "Decoración y hogar",
+    imagen: "https://images.unsplash.com/photo-1654856842821-f7a149e28a94?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZGVjb3JhY2lvbiUyMHklMjBob2dhcnxlbnwwfHwwfHx8MA%3D%3D",
     colSpan: 1,
     rowSpan: 1,
   },
   {
     nombre: "Juguetes",
-    imagen: "https://images.unsplash.com/photo-1583337130417-3346a1c137a7?auto=format&fit=crop&w=800&q=80",
+    imagen: "https://blog.lanasrubi.com/wp-content/uploads/2024/08/IMAGEN-DESTACADA-Patron-de-Amigurumi-Paso-a-Paso-para-Tejer-un-Adorable-Muneco-de-Lana.jpg",
     colSpan: 1,
     rowSpan: 1,
   },
   {
-    nombre: "Deportes",
-    imagen: "https://images.unsplash.com/photo-1599059817343-fcdba2f1d1af?auto=format&fit=crop&w=800&q=80",
+    nombre: "Regalos personalizados",
+    imagen: "https://plus.unsplash.com/premium_photo-1671395168227-37aa9724cd9e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHJlZ2Fsb3MlMjBhJTIwbWFub3xlbnwwfHwwfHx8MA%3D%3D",
     colSpan: 1,
     rowSpan: 1,
   },
   {
-    nombre: "Belleza",
-    imagen: "https://images.unsplash.com/photo-1588776814546-ec7ec12a778d?auto=format&fit=crop&w=800&q=80",
+    nombre: "Cosmética natural / Cuidado personal",
+    imagen: "https://images.unsplash.com/photo-1624454002302-36b824d7bd0a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y29zbWV0aWNhJTIwbmF0dXJhbHxlbnwwfHwwfHx8MA%3D%3D",
     colSpan: 1,
     rowSpan: 1,
   },
@@ -60,7 +60,7 @@ const CategoriasMosaico = () => {
         {categorias.map((cat, i) => (
           <div
             key={i}
-            className={`relative group overflow-hidden rounded-lg shadow hover:shadow-md transition ${getGridSpanClasses(
+            className={`relative group overflow-hidden rounded-lg shadow hover:shadow-md transition cursor-pointer ${getGridSpanClasses(
               cat.colSpan,
               cat.rowSpan
             )} min-h-[150px]`}
